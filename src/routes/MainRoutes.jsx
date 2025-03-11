a62a0e1cb3ba7027d367a6fe376519733c1c906b
drstsny <drstsny@gmail.com>
2025-03-11 09:34:39 +0700

    update cart

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -7,11 +7,8 @@ const Register = lazy(() => import("../pages/Register"));
 const Product = lazy(() => import("../pages/e-commerce/Product"));
 const ErrorPage = lazy(() => import("../notfound/NotFound"));
 const ListUser = lazy(() => import("../pages/ListUser"));
-<<<<<<< HEAD
 const Cart = lazy(() => import("../pages/e-commerce/Cart"))
-=======
 const AddProduct = lazy(() => import("../pages/e-commerce/AddProduct"))
->>>>>>> 1da974265e1f0e1777779d13892a24cc2a9bf458
 
 const router = createBrowserRouter([
     {
@@ -20,13 +17,10 @@ const router = createBrowserRouter([
         children: [
             { path: "products", element: <Product /> },
             { path: "user", element: <ListUser /> },
-<<<<<<< HEAD
             { path: "cart", element: <Cart/>},
-            { path: "profile", element: <ProfilePage /> },
+            // { path: "profile", element: <ProfilePage /> },
             { path: "*", element: <ErrorPage /> },
-=======
             { path: "*", element: <ErrorPage /> }, // Error Page tanpa sidebar? Pindahkan ke luar
->>>>>>> 1da974265e1f0e1777779d13892a24cc2a9bf458
         ],
     },
     { path: "login", element: <Login /> }, // Login dan Register di luar Layout (tanpa Sidebar)
